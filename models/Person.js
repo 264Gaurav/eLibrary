5434c4d5be2cec5bbab40e5cfb049bf38e4bce21
Gaurav Singh <gauravsingh264209@gmail.com>
2024-06-30 16:16:55 +0530

    Updated Person model with new fields

--- models/Person.js
+++ models/Person.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
 
     user:{
         type:String,
-        enum:['reader' ,  'admin' , 'author']
+        enum:['reader' , 'admin' , 'author']
     } ,
 
     phone:{
